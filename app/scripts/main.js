d53b5a672180ce73029d19f38bc2b10820a8d8d8
ikemesit <ikemesit@gmail.com>
2016-03-12 02:58:57 +0800

    changed video background, added booking form, to customize further

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
-console.log('\'Allo \'Allo!');
+
+$('input[name="daterange"]').daterangepicker(
+{
+    locale: {
+      format: 'YYYY-MM-DD'
+    },
+    autoUpdateInput: false,
+    singleDatePicker: true,
+    showDropdowns: true,
+    startDate: '2013-01-01',
+    endDate: '2013-12-31'
+}, 
+function(start, end) {
+	'use strict';
+    console.log('A new date range was chosen: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+});
