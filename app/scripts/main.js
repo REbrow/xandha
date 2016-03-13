76d82705af93ffb2b64ccfa4db3b76b4d28b6152
ikemesit <ikemesit@gmail.com>
2016-03-13 22:48:23 +0800

    initial mobile layout fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,7 +9,7 @@ $('input[name="daterange"]').daterangepicker(
     showDropdowns: true,
     startDate: '2013-01-01',
     endDate: '2013-12-31'
-}, 
+},
 function(start, end) {
 	'use strict';
     console.log('A new date range was chosen: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
