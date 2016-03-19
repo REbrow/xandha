adbd4daa7cfb63c6e2dfec5fb2a06d5e0262361c
ikemesit <ikemesit@gmail.com>
2016-03-19 20:19:34 +0800

    center page-header, remove page-header container padding

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,19 @@
 
-$('input[name="daterange"]').daterangepicker(
-{
-    locale: {
-      format: 'YYYY-MM-DD'
-    },
-    autoUpdateInput: false,
-    singleDatePicker: true,
-    showDropdowns: true,
-    startDate: '2013-01-01',
-    endDate: '2013-12-31'
-},
-function(start, end) {
-	'use strict';
-    console.log('A new date range was chosen: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
-});
+// $('input[name="daterange"]').daterangepicker(
+// {
+//     locale: {
+//       format: 'YYYY-MM-DD'
+//     },
+//     autoUpdateInput: false,
+//     singleDatePicker: true,
+//     showDropdowns: true,
+//     startDate: '2013-01-01',
+//     endDate: '2013-12-31'
+// },
+// function(start, end) {
+// 	'use strict';
+//     console.log('A new date range was chosen: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+// });
+
+
+$(".page-header").css("color: red");
